f14899d640811f442365fe8cc3fca152fe92c400
Joey Wiggs <jswiggs@gmail.com>
2021-07-03 19:43:29 -0700

    QuoteBot basic functionality complete: * When someone adds it to their server, it uploads the appropriate emoji and creates a channel * When someone adds the reaction to text item, it creates an embed with a link back to that item, shows the author, and shows the text

---
+++ DiscordQuoteBot/DiscordUtil.cs
@@ -0,0 +1,67 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordQuoteBot
+{
+	class DiscordUtil
+	{
+		public static SocketGuild GetGuildFromChannelId( DiscordSocketClient client, ulong snowflakeIdOfChannelReceived )
+		{
+			foreach ( var guild in client.Guilds )
+			{
+				if ( guild.Channels.Any( x => x.Id == snowflakeIdOfChannelReceived ) )
+				{
+					return guild;
+				}
+			}
+
+			return null;
+		}
+
+		public static async Task SendErrorToGuild( ulong snowflakeIdOfChannelReceived, SocketGuild guildReceived, string errorMessage )
+		{
+			var receivedChannel = guildReceived.Channels.FirstOrDefault( x => x.Id == snowflakeIdOfChannelReceived ) as SocketTextChannel;
+			if ( receivedChannel != null )
+			{
+				await receivedChannel.SendMessageAsync( errorMessage );
+			}
+			Console.WriteLine( $" { errorMessage } on server { guildReceived.Name }!" );
+		}
+
+		public static async Task<SocketTextChannel> GetQuoteBotChannelForGuild( SocketGuild guild, ulong snowflakeIdOfChannelReceived )
+		{
+			// Once we have the guild, let's get the QuoteBot channel
+			var channel = guild.Channels.FirstOrDefault( x => x.Name == "quotebot" );
+			if ( channel == null )
+			{
+				await SendErrorToGuild( snowflakeIdOfChannelReceived, guild, "No \"quotebot\" channel set up on server" );
+				return null;
+			}
+
+			var textChannel = channel as SocketTextChannel;
+			if ( textChannel == null )
+			{
+				await SendErrorToGuild( snowflakeIdOfChannelReceived, guild, $"\"quotebot\" channel is not a text channel" );
+				return null;
+			}
+
+			return textChannel;
+		}
+
+		public static bool ReactionIsQuoteBot( SocketReaction reaction )
+		{
+			return reaction.Emote.Name.Contains( quoteBotEmojiString );
+		}
+
+		public static bool MessageAlreadyHasReaction( IUserMessage message )
+		{
+			var reaction = message.Reactions.FirstOrDefault( x => x.Key.Name.Contains( quoteBotEmojiString ) );
+			return reaction.Key != null && reaction.Value.ReactionCount > 1;
+		}
+
+		public static string quoteBotEmojiString = "quotebot";
+	}
+}
--- DiscordQuoteBot/Program.cs
+++ DiscordQuoteBot/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace DiscordQuoteBot
 {
@@ -12,15 +14,154 @@ namespace DiscordQuoteBot
 
 		public async Task MainAsync()
 		{
-			m_client = new DiscordSocketClient();
+			var config = new DiscordSocketConfig { MessageCacheSize = 100 };
+			m_client = new DiscordSocketClient( config );
 			m_client.Log += Log;
 
 			var token = Environment.GetEnvironmentVariable( "DiscordQuoteBotToken", EnvironmentVariableTarget.User );
 			await m_client.LoginAsync( TokenType.Bot, token );
 			await m_client.StartAsync();
 
+			m_client.ReactionAdded += ReactionAdded;
+			m_client.JoinedGuild += JoinedGuild;
+			foreach ( var guild in m_client.Guilds )
+			{
+				var quoteBotChannel = guild.Channels.First( x => x.Name == "quotebot" );
+			}
+
 			// Block this task until the program is closed.
 			await Task.Delay( -1 );
+		}
+
+		private async Task JoinedGuild( SocketGuild guild )
+		{
+			// If the emote we use doesn't exist, make it
+			var emote = guild.Emotes.FirstOrDefault( x => x.Name == DiscordUtil.quoteBotEmojiString );
+			if ( emote == null )
+			{
+				Image icon = new Image( "Data\\quotemark.png" );
+				await guild.CreateEmoteAsync( DiscordUtil.quoteBotEmojiString, icon );
+				Console.WriteLine( $"Created QuoteBot Emoji for guild { guild.Name }" );
+			}
+			else
+			{
+				Console.WriteLine( $"QuoteBot Emoji already exists for guild { guild.Name }" );
+			}
+
+			// If the channel we use doesn't exist, make it
+			var channel = guild.Channels.FirstOrDefault( x => ( x.Name == DiscordUtil.quoteBotEmojiString ) && ( x as SocketTextChannel != null ) );
+			if ( channel == null )
+			{
+				await guild.CreateTextChannelAsync( DiscordUtil.quoteBotEmojiString, x =>
+				{
+					List<Overwrite> permissionOverwrites = new List<Overwrite>( 2 );
+
+					// Set Everyone to read only
+					var everybodyRolePermissions = new OverwritePermissions(
+						PermValue.Inherit, // CreateInstantInvite
+						PermValue.Inherit, // ManageChannel
+						PermValue.Inherit, // addReactions
+						PermValue.Inherit, // viewChannel
+						PermValue.Deny, // sendMessages
+						PermValue.Inherit, // sendTTSMessages
+						PermValue.Inherit, // manageMessages
+						PermValue.Deny, // embedLinks
+						PermValue.Deny, // attachFiles
+						PermValue.Inherit, // readMessageHistory
+						PermValue.Deny, // mentionEveryone
+						PermValue.Inherit, // useExternalEmojis
+						PermValue.Inherit, // connect
+						PermValue.Inherit, // speak
+						PermValue.Inherit, // muteMembers
+						PermValue.Inherit, // deafenMembers
+						PermValue.Inherit, // moveMembers
+						PermValue.Inherit, // useVoiceActivation
+						PermValue.Inherit, // manageRoles
+						PermValue.Inherit, // manageWebhooks
+						PermValue.Inherit, // prioritySpeaker
+						PermValue.Inherit  // stream
+						);
+
+					// Allow the bot to write
+					var botUserPermissions = new OverwritePermissions(
+						PermValue.Inherit, // CreateInstantInvite
+						PermValue.Inherit, // ManageChannel
+						PermValue.Inherit, // addReactions
+						PermValue.Inherit, // viewChannel
+						PermValue.Allow, // sendMessages
+						PermValue.Inherit, // sendTTSMessages
+						PermValue.Inherit, // manageMessages
+						PermValue.Allow, // embedLinks
+						PermValue.Deny, // attachFiles
+						PermValue.Inherit, // readMessageHistory
+						PermValue.Deny, // mentionEveryone
+						PermValue.Inherit, // useExternalEmojis
+						PermValue.Inherit, // connect
+						PermValue.Inherit, // speak
+						PermValue.Inherit, // muteMembers
+						PermValue.Inherit, // deafenMembers
+						PermValue.Inherit, // moveMembers
+						PermValue.Inherit, // useVoiceActivation
+						PermValue.Inherit, // manageRoles
+						PermValue.Inherit, // manageWebhooks
+						PermValue.Inherit, // prioritySpeaker
+						PermValue.Inherit  // stream
+						);
+
+
+					permissionOverwrites.Add( new Overwrite( guild.EveryoneRole.Id, PermissionTarget.Role, everybodyRolePermissions ) );
+					permissionOverwrites.Add( new Overwrite( m_client.CurrentUser.Id, PermissionTarget.User, botUserPermissions ) );
+
+					x.PermissionOverwrites = permissionOverwrites;
+				} );
+
+				Console.WriteLine( $"Created QuoteBot Channel for guild { guild.Name }" );
+			}
+			else
+			{
+				Console.WriteLine( $"QuoteBot Channel already exists for guild { guild.Name }" );
+			}
+		}
+
+		private async Task ReactionAdded( Cacheable<IUserMessage, ulong> cachedMessage, ISocketMessageChannel originChannel, SocketReaction reaction )
+		{
+			if ( !DiscordUtil.ReactionIsQuoteBot( reaction ) )
+			{
+				return;
+			}
+
+			var message = await cachedMessage.GetOrDownloadAsync();
+			if ( DiscordUtil.MessageAlreadyHasReaction( message ) )
+			{
+				// Don't add a quote if folks keep adding their own values
+				return;
+			}
+
+			var guild = DiscordUtil.GetGuildFromChannelId( m_client, originChannel.Id );
+			if ( guild == null )
+			{
+				Console.WriteLine( $" No guild found for Channel Id { originChannel.Id }!" );
+				return;
+			}
+
+			var botChannel = await DiscordUtil.GetQuoteBotChannelForGuild( guild, originChannel.Id );
+			if ( botChannel == null )
+			{
+				return;
+			}
+
+			string link = $"https://discord.com/channels/{ guild.Id }/{ originChannel.Id }/{ message.Id }";
+
+			var author = new EmbedAuthorBuilder()
+				.WithName( message.Author.Username )
+				.WithIconUrl( message.Author.GetAvatarUrl() );
+			EmbedBuilder builder = new EmbedBuilder
+			{
+				Url = link,
+				Title = message.Content,
+				Author = author
+			};
+			await botChannel.SendMessageAsync( null, false, builder.Build() );
 		}
 
 		private Task Log( LogMessage msg )
