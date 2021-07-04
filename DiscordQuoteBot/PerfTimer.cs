742f1d4e78e0d5b9feee37381edca54c1018326e
Joey Wiggs <jswiggs@gmail.com>
2021-07-04 12:21:38 -0700

    Add RandomQuoteModule for command !randomquote Add perftimer because my current version of randomquote is gonna be sloooow.

---
+++ DiscordQuoteBot/PerfTimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DiscordQuoteBot
+{
+	public class PerfTimer : IDisposable
+	{
+		private Stopwatch m_stopwatch;
+		private string m_eventName;
+
+		public PerfTimer( string eventName )
+		{
+			m_stopwatch = Stopwatch.StartNew();
+			m_eventName = eventName;
+		}
+
+		public void Dispose()
+		{
+			m_stopwatch.Stop();
+			Console.WriteLine( $"PerfTimer for { m_eventName }: { m_stopwatch.ElapsedMilliseconds } milliseconds" );
+		}
+	}
+}
---
+++ DiscordQuoteBot/RandomQuoteModule.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordQuoteBot
+{
+	public class RandomQuoteModule : ModuleBase<SocketCommandContext>
+	{
+		[Command( "randomquote" )]
+		[Summary( "Grabs a random quote from the #quotebot channel" )]
+		public async Task RandomQuoteAsync()
+		{
+			Console.WriteLine( $"RandomQuote Called for { Context.Guild.Name } in Channel { Context.Channel.Name }" );
+
+			// Ooof, this is gonna get expensive, also limited to last 500 messages
+			// Longer term, we're going to need a way to build our own cache and fill it slowly over time
+			// To grab random quotes from
+
+			using ( PerfTimer perfTimer = new PerfTimer( "RandomQuoteAsync" ) )
+			{
+				var quoteBotChannel = await DiscordUtil.GetQuoteBotChannelForGuild( Context.Guild, Context.Channel.Id );
+				var messages = await quoteBotChannel.GetMessagesAsync( 500 ).FlattenAsync();
+				var messageList = messages.ToList();
+				var random = new Random();
+
+				var randomMessage = messageList[ random.Next( 0, messageList.Count ) ];
+				var embed = randomMessage.Embeds.First() as Embed;
+
+				await Context.Channel.SendMessageAsync( null, false, embed );
+			}
+		}
+	}
+}
